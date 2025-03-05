4b45b66d925937a2ccd29602ce9a9ea74a7156cb
leanSanchez-Dev <leansanchez14@gmail.com>
2025-03-05 00:17:36 -0500

    create ejercice 05

--- lib/pages/excercices/exercice_detail_screen.dart
+++ lib/pages/excercices/exercice_detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:ejercicios_logica/pages/excercices/exercice_01.dart';
 import 'package:ejercicios_logica/pages/excercices/exercice_02.dart';
 import 'package:ejercicios_logica/pages/excercices/exercice_03.dart';
 import 'package:ejercicios_logica/pages/excercices/exercice_04.dart';
+import 'package:ejercicios_logica/pages/excercices/exercice_05.dart';
 import 'package:ejercicios_logica/utils/data/morse_code.dart';
 import 'package:http/http.dart' as http;
 import 'package:image/image.dart' as img;
@@ -110,7 +111,7 @@ class _ExerciceDetailScreenState extends State<ExerciceDetailScreen> {
       case 4:
         return Expanded(child: Exercice04());
       case 5:
-        return Expanded(child: exercice5());
+        return Expanded(child: Exercice05());
       case 6:
         return Expanded(child: exercice6());
       case 7:
@@ -128,136 +129,6 @@ class _ExerciceDetailScreenState extends State<ExerciceDetailScreen> {
       default:
         return Center(child: Text('No hay contenido para este ejercicio'));
     }
-  }
-
-  double calcularArea(
-    String tipo, {
-    double base = 0,
-    double altura = 0,
-    double lado = 0,
-  }) {
-    switch (tipo.toLowerCase()) {
-      case "triangulo":
-        return (base * altura) / 2;
-      case "cuadrado":
-        return lado * lado;
-      case "rectangulo":
-        return base * altura;
-      default:
-        return 0;
-    }
-  }
-
-  String selectedPolygon = "triangulo";
-  double base = 0, altura = 0, lado = 0;
-  double area = 0;
-
-  exercice5() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          'Cálculo del área de polígonos',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 10),
-        DropdownButton<String>(
-          padding: EdgeInsets.all(10),
-          isDense: true,
-          // isExpanded: true,
-          dropdownColor: Colors.white,
-          alignment: Alignment.center,
-
-          icon: const Icon(Icons.arrow_drop_down_rounded),
-          // ignore: deprecated_member_use
-          iconEnabledColor: Color(Colors.deepPurpleAccent.value),
-          value: selectedPolygon,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          underline: Container(
-            height: 2,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.deepPurpleAccent,
-            ),
-          ),
-          items:
-              ["triangulo", "cuadrado", "rectangulo"]
-                  .map(
-                    (e) => DropdownMenuItem(
-                      alignment: Alignment.center,
-                      value: e,
-                      child: Text(e),
-                    ),
-                  )
-                  .toList(),
-          onChanged: (value) {
-            setState(() {
-              selectedPolygon = value!;
-              area = calcularArea(
-                selectedPolygon,
-                base: base,
-                altura: altura,
-                lado: lado,
-              );
-            });
-          },
-        ),
-        if (selectedPolygon != "cuadrado")
-          TextField(
-            decoration: InputDecoration(labelText: 'Base'),
-            keyboardType: TextInputType.number,
-            onChanged: (value) {
-              setState(() {
-                base = double.tryParse(value) ?? 0;
-                area = calcularArea(
-                  selectedPolygon,
-                  base: base,
-                  altura: altura,
-                  lado: lado,
-                );
-              });
-            },
-          ),
-        if (selectedPolygon != "cuadrado")
-          TextField(
-            decoration: InputDecoration(labelText: 'Altura'),
-            keyboardType: TextInputType.number,
-            onChanged: (value) {
-              setState(() {
-                altura = double.tryParse(value) ?? 0;
-                area = calcularArea(
-                  selectedPolygon,
-                  base: base,
-                  altura: altura,
-                  lado: lado,
-                );
-              });
-            },
-          ),
-        if (selectedPolygon == "cuadrado")
-          TextField(
-            decoration: InputDecoration(labelText: 'Lado'),
-            keyboardType: TextInputType.number,
-            onChanged: (value) {
-              setState(() {
-                lado = double.tryParse(value) ?? 0;
-                area = calcularArea(
-                  selectedPolygon,
-                  base: base,
-                  altura: altura,
-                  lado: lado,
-                );
-              });
-            },
-          ),
-        const SizedBox(height: 10),
-        Text(
-          'Área calculada: $area',
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 20),
-      ],
-    );
   }
 
   String aspectRatio = '';
